d3c8defeb827511283d59816fa2317a385db92df
AnjinIsmail <anjin.ismail@gmail.com>
2020-12-10 12:53:24 -0800

    added parties table

--- db/schema.sql
+++ db/schema.sql
@@ -6,9 +6,8 @@ CREATE TABLE candidates (
 );
 
 
-CREATE TABLE candidates (
+CREATE TABLE parties (
   id INTEGER PRIMARY KEY,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  industry_connected BOOLEAN NOT NULL
+  name VARCHAR(50)NOT NULL,
+  description TEXT
 );
